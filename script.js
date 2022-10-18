001faf99fec6e9d93df18efcb35e7663d80b0bf6
musaliyah <asmusaliar@gmail.com>
2022-10-17 20:54:37 -0700

    reorganizing

--- script.js
+++ script.js
@@ -25,70 +25,75 @@ var mainDivEl = document
 var timer;
 //integer variable 
 var timerCount; //the amount of time that timer is going to run
-// var startButton = document.querySelector(".start-button")
-// var isWin = false; 
-
-// boolean variable 
-// put question answers in an array variable
-// can track through question number variable 
-// could use an array method for each one but difficult
-// an array could be a list of objects 
-// const questions = [
-    // {question: "this is a question",
-    // answer1: "this is answer1",
-    // answer2: "this is answer 2"},
-    // {question: "this is question 2",
-    //  answer1: "this is answer1"},
-    //  etc];
-
-// document.getElementById("button1").textContent = questions[0].answer1
-
-const startButton = document.getElementById('start-button');
-const quizContainer = document.getElementById('quiz-container');
 
 
-startButton.addEventListener('click', startGame)
+var questions = {
+    questionList: {
+        1:'Commonly used data types DO NOT Include:',
+        2:'The condition in an if/else statement is enclosed within ____',
+        3:'Arrays in JavaScript can be used to store ____.',
+        4:'A very useful tool used during development and debugging for printing content to the debugger is:'
+    }
+};
 
-
-const questions = [
-    {question:"Commonly used data types DO NOT Include:",
-        options: {
+var choices = {
+    options: {
+        1: {
             answer1:"strings",
             answer2:"booleans",
             answer3: "alerts",
             answer4: "numbers"
-        }, 
-        correct: "alerts" 
-    }, 
-    {question:"The condition in an if/else statement is enclosed within ____",
-    options: {
-        answer1:"quotes",
-        answer2:"curly brackets",
-        answer3: "parenthesis",
-        answer4: "A square brackets"
-    }, 
-    correct: "curly brackets"
-    },
-    {question:"Arrays in JavaScript can be used to store ____.",
-    options: {
-        answer1:"numbers and strings",
-        answer2:"other arrays",
-        answer3: "booleans",
-        answer4:"all of the above"
-    }, 
-    correct: "all of the above"
-    },
-    {question:"A very useful tool used during development and debugging for printing content to the debugger is:",
-    options: {
-        answer1:"JavaScript",
-        answer2:"terminal",
-        answer3: "for loops",
-        answer4:"console.log"
-    }, 
-    correct: "console.log"
+        }
     }
+}
+// var  = {
+//     right: {
+//         1: "alerts",
+//         2: "curly brackets",
+//         3: 'all of the above',
+//         4: 'console.log'
+//     }
+// }
 
-] //Recieved advice/guidence about question array from Tutor (Andrew Tirpok)
+// const questions = [
+//     {question:"Commonly used data types DO NOT Include:",
+//         1: {
+//             answer1:"strings",
+//             answer2:"booleans",
+//             answer3: "alerts",
+//             answer4: "numbers"
+//         }, 
+//         correct: "alerts" 
+//     }, 
+//     {question:"The condition in an if/else statement is enclosed within ____",
+//     2: {
+//         answer1:"quotes",
+//         answer2:"curly brackets",
+//         answer3: "parenthesis",
+//         answer4: "A square brackets"
+//     }, 
+//     correct: "curly brackets"
+//     },
+//     {question:"Arrays in JavaScript can be used to store ____.",
+//     3: {
+//         answer1:"numbers and strings",
+//         answer2:"other arrays",
+//         answer3: "booleans",
+//         answer4:"all of the above"
+//     }, 
+//     correct: "all of the above"
+//     },
+//     {question:"A very useful tool used during development and debugging for printing content to the debugger is:",
+//    4: {
+//         answer1:"JavaScript",
+//         answer2:"terminal",
+//         answer3: "for loops",
+//         answer4:"console.log"
+//     }, 
+//     correct: "console.log"
+//     }
+
+// ] //Recieved advice/guidence about question array from Tutor (Andrew Tirpok)
 
 function startGame(){
     console.log('Game Start')
