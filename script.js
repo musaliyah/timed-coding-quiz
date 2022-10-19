44ac88e7176460e3b96f8673eef8b22829f79729
musaliyah <asmusaliar@gmail.com>
2022-10-19 16:44:16 -0700

    timer function

--- script.js
+++ script.js
@@ -23,18 +23,71 @@ var questionList = [
         correct: 3
     },
     {
-        questionText: 'Commonly used data types DO NOT Include:',
-        answerOptions: ['1) strings', '2) booleans', '3) alerts', '4) numbers'],
+        questionText: 'The condition in an if/else statement is enclosed within ____',
+        answerOptions: ['1) quotes', '2) curly brackets', '3) parenthesis', '4) A square brackets'],
         correct: 3
     },
     {
-        questionText: 'Commonly used data types DO NOT Include:',
-        answerOptions: ['1) strings', '2) booleans', '3) alerts', '4) numbers'],
-        correct: 3
+        questionText: 'Arrays in JavaScript can be used to store ____.',
+        answerOptions: ['1) numbers and strings', '2) other arrays', '3) booleans', '4) all of the above'],
+        correct: 4
     },
     {
-        questionText: 'Commonly used data types DO NOT Include:',
-        answerOptions: ['1) strings', '2) booleans', '3) alerts', '4) numbers'],
-        correct: 3
-    },
-]
+        questionText: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        answerOptions: ['1) JavaScript', '2) terminal', '3) for loops', '4) console.log'],
+        correct: 4
+    }
+];
+
+var countdown = setInterval(countdownTimer, 1000);
+
+function countdownTimer() {
+    if(timeStart)
+    time --;
+    if(time <=0){
+    quizOver();
+    time = 0;
+    }
+    document.getElementById("time").innerHTML = time;
+}
+
+beginQuizBtn.addEventListener("click", function() {
+    quizContainer.style.display = "block";
+    informationContainer.style.display = "none";
+    countdownTimer.style.display= "block";
+    document.getElementById("scores").style.display= "block";
+    document.getElementById("score").innerHTML = score;
+    countdownTimer();
+    quizQuestions();
+    timeStart = true;
+});
+
+function quizQuestions() {
+    question.textContent = questionList[i].questionText;
+    answer1.textContent = questionList[i].answerOptions[0];
+    answer2.textContent = questionList[i].answerOptions[1];
+    answer3.textContent = questionList[i].answerOptions[2];
+    answer4.textContent = questionList[i].answerOptions[3];
+};
+
+answer1.addEventListener('click', function(event) {
+    event.endDisplay();
+    correct = questionList[i].correct;
+    console.log("correct" + correct);
+    if(0 === correct) {
+        document.getElementById("answers").innerHTML = "Right!";
+        setTimeout(function() {
+            document.getElementById("answers").innerHTML = "";
+        }, 
+        1000
+        );
+        score++;
+        document.getElementById("score").innerHTML= score;
+    } else {
+        timeRemaining -= 5;
+        document.getElementById("answer").innerHTML = "Wrong!";
+        setTimeout(function() {
+            document.
+        })
+    }
+})
