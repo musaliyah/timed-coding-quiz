61264c9aba403b6a119f25a9a3090521b14a47ee
musaliyah <asmusaliar@gmail.com>
2022-10-19 16:06:53 -0700

    simplified html

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ var beginQuizBtn = document.getElementById('begin');
 var time = 80;
 var timeRemaining = true;
 var timeStart = false;
+var timer = document.getElementById('time');
+var informationContainer = document.getElementById('information-container');
+var 
